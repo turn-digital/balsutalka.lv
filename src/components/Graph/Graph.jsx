5026e45e195463bbc4f1ef6064032f0690d0d4b6
Reinis <reinis@turn.lv>
2023-04-27 22:57:17 +0300

    graph component

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,32 +1,53 @@
 const Graph = (props) => {
-  console.log("Section1");
   return (
-    <>
-      <div
-        style={{
-          background: "#164460",
-        }}
-      >
-        <h1>Palīdzi attīstīt latviešu valodas tehnoloģijas ar savu balsi!</h1>
-        <div style={{ display: "flex" }}>
-          <div>
-            <p>Izaicinājums</p>
-            <h2>Ierunāsim 100 stundas 4. maijā!</h2>
+    <section className="graph">
+      <div className="wrapper">
+        <h2 className="graph__title title">
+          Palīdzi attīstīt latviešu valodas tehnoloģijas ar savu balsi!
+        </h2>
+        <div className="graph__box">
+          <div className="graph__col">
+            <p className="graph__info">
+              <span>Izaicinājums</span>Ierunāsim 100 stundas 4. maijā!
+            </p>
           </div>
-          <div>
-            <p>
-              Latviešu <img src="/public/assets/images/svg/latvija.svg"></img>
-            </p>
-            <p>
-              Igauņu<img src="/public/assets/images/svg/igaunu.svg"></img>
-            </p>
-            <p>
-              Lietuviešu<img src="/public/assets/images/svg/lietuva.svg"></img>
-            </p>
+          <div className="graph__col">
+            <div className="graph__table">
+              <div className="graph__row">
+                <div className="graph__stat">
+                  <span>Latviešu</span>
+                  <span>18/100 h</span>
+                </div>
+                <img
+                  src="/assets/images/svg/latvija.svg"
+                  alt="Latvijai ir 18h attēlots stabiņu diagrammā"
+                ></img>
+              </div>
+              <div className="graph__row">
+                <div className="graph__stat">
+                  <span>Igauņu</span>
+                  <span>56/100 h</span>
+                </div>
+                <img
+                  src="/assets/images/svg/igaunu.svg"
+                  alt="Igaunijai ir 56h attēlots stabiņu diagrammā"
+                ></img>
+              </div>
+              <div className="graph__row">
+                <div className="graph__stat">
+                  <span>Lietuviešu</span>
+                  <span>54/100 h</span>
+                </div>
+                <img
+                  src="/assets/images/svg/lietuva.svg"
+                  alt="Lietuvai ir 54h attēlots stabiņu diagrammā"
+                ></img>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
