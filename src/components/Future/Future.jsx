bc91d36a37efbb7f70e05542d01c0f31dd1d8f8e
Reinis <reinis@turn.lv>
2023-04-27 21:16:15 +0300

    add some future section style need images

--- src/components/Future/Future.jsx
+++ src/components/Future/Future.jsx
@@ -1,23 +1,48 @@
 const Future = (props) => {
   return (
     <section className="future">
-      <div>
-        <h1>Ieguldi latviešu valodas nākotnē!</h1>
-        <div style={{ display: "flex" }}>
-          <div>
-            <h1>Runā</h1>
-            <p>
-              Ziedo savu balsi līdzās tūkstošiem citu! Jo vairāk un
-              daudzveidīgāki būs balsu paraugi, jo ātrāk un veiksmīgāk izdosies
-              attīstīt runas tehnoloģijas latviešu valodā!
-            </p>
+      <div className="wrapper">
+        <h2 className="future__title title">
+          Ieguldi latviešu valodas nākotnē!
+        </h2>
+        <div className="future__content">
+          <div className="future__card">
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener"
+              title="Atvērt runā saiti"
+            ></a>
+            <h3 className="future__subtitle subtitle">Runā</h3>
+            <div className="future__box">
+              <div className="future__text text-editor">
+                <p>
+                  Ziedo savu balsi līdzās tūkstošiem citu! Jo vairāk un
+                  daudzveidīgāki būs balsu paraugi, jo ātrāk un veiksmīgāk
+                  izdosies attīstīt runas tehnoloģijas latviešu valodā!
+                </p>
+              </div>
+              <img src="/assets/images/svg/mic.svg"></img>
+            </div>
           </div>
-          <div>
-            <h1>Klausies</h1>
-            <p>
-              Palīdzi novērtēt, kā ierunājuši citi! Ierakstu apstiprināšana ir
-              vienlīdz svarīga, lai radītu kvalitatīvus un atvērtus runas datus.
-            </p>
+          <div className="future__card">
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener"
+              title="Atvērt klausies saiti"
+            ></a>
+            <h3 className="future__subtitle subtitle">Klausies</h3>
+            <div className="future__box">
+              <div className="future__text text-editor">
+                <p>
+                  Palīdzi novērtēt, kā ierunājuši citi! Ierakstu apstiprināšana
+                  ir vienlīdz svarīga, lai radītu kvalitatīvus un atvērtus runas
+                  datus.
+                </p>
+              </div>
+              <img src="/assets/images/svg/listen.svg"></img>
+            </div>
           </div>
         </div>
       </div>
