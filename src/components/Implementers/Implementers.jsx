59178d391c49bfeac9e28c5929132fd9070551e9
Raivis Dejus <raivisd@scandiweb.com>
2023-05-27 21:25:21 +0300

    Reorganized logos

--- src/components/Implementers/Implementers.jsx
+++ src/components/Implementers/Implementers.jsx
@@ -7,39 +7,57 @@ const Implementers = (props) => {
           <img
             src="/assets/images/LUMII.png"
             alt="LUMII logo"
-            width="282px"
-            height="160px"
+            width="282"
+            height="160"
           />
           <img
             src="/assets/images/LULFMI.png"
             alt="LULFMI logo"
-            width="282px"
-            height="160px"
+            width="282"
+            height="160"
           />
           <img
             src="/assets/images/LATA.png"
             alt="LATA logo"
-            width="282px"
-            height="160px"
+            width="282"
+            height="160"
           />
           <img
             src="/assets/images/UnescoLV.png"
             alt="UnescoLV logo"
-            width="282px"
-            height="160px"
+            width="282"
+            height="160"
           />
+        </div>
+      </div>
+      <div className="wrapper">
+        <p className="implementers__title title">Projekta atbalstītāji</p>
+        <div className="implementers__logos">
           <img
             src="/assets/images/svg/DzDsv.svg"
             alt="DzDsv logo"
-            width="282px"
-            height="160px"
+            width="282"
+            height="160"
           />
           <img
             src="/assets/images/LNKC.png"
             alt="LNKC logo"
-            width="282px"
-            height="160px"
+            width="282"
+            height="160"
           />
+
+          <div className="implementers__logos-es_logo">
+            <img
+              src="/assets/images/svg/ESLogo.svg"
+              width="220"
+              height="48"
+              alt="ES atbalsta logo"
+            />
+            <span>
+              Balsu talkas organizēšanu līdzfinansē Eiropas Savienības Atveseļošanas un noturības mehānisma investīcija un valsts budžets projekta “Valodu tehnoloģiju iniciatīva” (2.3.1.1.i.0/1/22/I/CFLA/002) ietvaros.
+            </span>
+          </div>
+
         </div>
       </div>
     </section>
