891a262a4a4203ccf8c69366bc4c0e7fea8a8bf7
Raivis Dejus <orvils@gmail.com>
2025-04-07 19:13:08 +0300

    Adding latgalian translation

--- src/components/Results/ResultsLTG.jsx
+++ src/components/Results/ResultsLTG.jsx
@@ -3,17 +3,15 @@ const ResultsLTG = (props) => {
     <section className="results">
       <div className="wrapper">
         <p className="results__title title">
-          Sasniegtie rezultāti
+          Sasnāgtī rezultati
         </p>
         <div className="results__content text-editor">
           <div className="results__text">
-            <p>Lai arī darbs pie Balsu talkas datu vākšanas un apstrādes joprojām notiek, līdz šim savāktie balsu ieraksti
-              jau tiek izmantoti un daži rezultāti ir pieejami publiski.
+            <p>Lai ari dorbs pi Bolsu tolkys datu vuokšonys i apstruodis aizviņ nūteik, da šam savuoktī bolsu īroksti jau teik lītuoti, i puors rezultati ir daīmami publiski.
               <ul>
-                <li><a target="_blank" href="https://ailab.lv/">LU MII AiLab</a> ir izveidojuši <a target="_blank"
-                                                                                                   href="https://huggingface.co/AiLab-IMCS-UL/whisper-large-v3-latgalian-2503">jaudīgu
-                  runas atpazīšanas modeli</a> ar salīdzinoši augstu atpazīšanas precizitāti runas datu analīzei un
-                  subtitru veidošanai. <br/>Šo modeli var izmēģināt <a target="_blank" href="http://ltg.late.ailab.lv/">ltg.late.ailab.lv</a>.
+                <li><a target="_blank" href="https://ailab.lv/">LU MII AiLab</a> ir sataisejuši <a target="_blank"
+                                                                                                   href="https://huggingface.co/AiLab-IMCS-UL/whisper-large-v3-latgalian-2503">jaudeigu runys atpazeišonys modeli</a> ar saleidzynūši augstu atpazeišonys precizitati runys datu analizei i subtitru izveiduošonai.
+Reiku, kurs izstruoduots iz ituo modeļa bāzis, var paraudzeit <a target="_blank" href="http://ltg.late.ailab.lv/">ltg.late.ailab.lv</a>.
                 </li>
               </ul>
             </p>
